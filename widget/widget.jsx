8d06f8965163f6a6c616204d22b53a504a5ffe9f
Jonathan Wong <96433015+JonJWong@users.noreply.github.com>
2023-03-23 22:24:41 -0400

    Adjust JSX to match .js, code cleanup + refactor (#4)
    * Update state with entrant, ladder
    
    * extract DEFAULT_STATE, fix variable naming
    
    * Change config to CONFIG, fix single/double quote inconsistency
    
    * Remove interval from window, change string interpolation
    
    * Add load guard, code cleanup
    
    * Format and cleanup .js, .jsx
    
    * Refactor DEFAULT_STATE, ladder, reformat .js
    
    * Change to function declaration for hoisting
    
    * DRY up js
    
    * Rename LADDER_ROW to EMPTY_LADDER_ENTRY, DRY up jsx
    
    * Remove loading from js
    
    * Remove magic number for ladder length

--- widget/widget.jsx
+++ widget/widget.jsx
@@ -1,195 +1,224 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+
+/* Change the number here to be the same as your ITL entrant id
+  ex. ENTRANT_ID = 99; */
+const ENTRANT_ID = 41;
+
+const REFRESH_INTERVAL = 60000; // 60 seconds in milliseconds
 
 const CONFIG = {
-  // Change this to be the same as your ITL entrant id
-  entrant_id: 41,
- 
-  endpoint: "https://itl2023.groovestats.com/api/widget/",
+  endpoint: `https://itl2023.groovestats.com/api/entrant/${ENTRANT_ID}/stats`,
 
-  // Use this to override the name that displays on the widget
-  // Useful if your ITL/GS name is over 11 characters
-  override_name: "",
+  /* Use this to override the name that displays on the widget.
+    Useful if your ITL/GS name is over 11 characters, or if you prefer
+    a different handle. */
+  overrideName: "",
 
-  // Use this to override the avatar source
-  // Useful if you want to use a non-png file as an avatar
-  avatar_source: "",
+  /* Use this to override the avatar source.
+    Useful if you want to use a non-png file as an avatar.
+    Format should be a URL. ex. "https://giphy.com/imageurl.gif" */
+  avatarSource: "",
+};
+
+const EMPTY_LADDER_ENTRY = {
+  rank: "--",
+  name: "--",
+  rankingPoints: 0,
+  difference: 0,
+  type: "neutral",
 }
 
-const REFRESH_INTERVAL = 60000 // 60 seconds in milliseconds
+const DEFAULT_STATE = {
+  entrant: {
+    id: "--",
+    name: "---",
+    rank: "---",
+    rankingPoints: "--",
+    totalPoints: "--",
+    totalPass: "---",
+    totalFc: "--",
+    totalFec: "--",
+    totalQuad: "--",
+    totalQuint: "--",
+    jackLevel: "-",
+    crossoverLevel: "-",
+    bracketLevel: "-",
+    footswitchLevel: "-",
+    sideswitchLevel: "-",
+    doublestepLevel: "-",
+    staminaLevel: "-",
+  },
+
+  ladder: createLadder(6),
+};
+
+function createLadder(num) {
+  const ladderArray = [];
+  for (let i = 0; i < num; i++) {
+    ladderArray.push(EMPTY_LADDER_ENTRY)
+  }
+  return ladderArray;
+}
+
+function formatDifference(difference) {
+  return difference === 0
+    ? "--"
+    : difference > 0
+      ? `+${difference}`
+      : `${difference}`;
+};
 
 const ITLWidget = () => {
-  const [state, setState] = useState({
-    "id": "--",
-    "name": "--",
-    "rank": "---",
-    "rankingPoints": "--",
-    "totalPoints": "--",
-    "totalPass": "---",
-    "totalFC": "--",
-    "totalFec": "--",
-    "totalQuad": "--",
-    "totalQuint": "-",
-    "jackLevel": "-",
-    "crossoverLevel": "-",
-    "bracketLevel": "-",
-    "footswitchLevel": "-",
-    "sideswitchLevel": "-",
-    "doublestepLevel": "-",
-    "staminaLevel": "-",
-    "ladder": [
-      {"rank": "--", "name": "--", "rankingPoints": 0, "difference": 0, "type": "neutral"},
-      {"rank": "--", "name": "--", "rankingPoints": 0, "difference": 0, "type": "neutral"},
-      {"rank": "--", "name": "--", "rankingPoints": 0, "difference": 0, "type": "neutral"},
-      {"rank": "--", "name": "--", "rankingPoints": 0, "difference": 0, "type": "neutral"},
-      {"rank": "--", "name": "--", "rankingPoints": 0, "difference": 0, "type": "neutral"},
-      {"rank": "--", "name": "--", "rankingPoints": 0, "difference": 0, "type": "neutral"},
-    ]
-  });
+  const [state, setState] = useState(DEFAULT_STATE);
+  const [loaded, setLoaded] = useState(false);
 
-  const format_difference = (diff) => {
-    if (diff == 0) {
-      return "--";
-    } 
-    else if (diff > 0) {
-      return "+" + diff;
-    }
-    else {
-      return diff;
-    }
-  }
-
-  const get_info = () => {
-    fetch(CONFIG.endpoint + CONFIG.entrant_id + "/stats")
+  const getInfo = () => {
+    fetch(CONFIG.endpoint)
       .then((response) => {
-        if (response.ok) { 
-          var data = response.json();
-          
-          // calculate the ranking points difference between the entrant_id and the rest of the ladder
-          for (var i = 0; i < 6; i++) { 
-            data.ladder[i]['difference'] = data.rankingPoints - data.ladder[i]['rankingPoints'];
-          }
-  
-          return data;
-         }
-        return Promise.reject(response); 
+        if (response.ok) {
+          const json = response.json();
+          return json;
+        }
+
+        return Promise.reject(response);
       })
-      .then((data) => {
+      .then((json) => {
+        const data = json.data;
+
+        // Calculate the ranking points difference between the ENTRANT_ID and the rest of the ladder
+        for (let i = 0; i < 6; i++) {
+          data.ladder[i].difference =
+            data.entrant.rankingPoints - data.ladder[i].rankingPoints;
+        }
+
         setState(data);
+        setLoaded(true);
       })
       .catch((error) => {
-        console.error('Error', error);
-      })
-  }
+        console.error("Error", error);
+      });
+  };
 
   useEffect(() => {
     // runs at component mount
-    window.componentInterval = setInterval(get_info(), REFRESH_INTERVAL);
+    getInfo();
+    const refreshInterval = setInterval(() => getInfo(), REFRESH_INTERVAL);
 
     return () => {
       // runs at component un-mount
-      clearInterval(window.componentInterval)
-    }
-  })
+      clearInterval(refreshInterval);
+    };
+
+    /* Will run once on mount, and then whenever getInfo changes (never).
+      Still runs on dismount with return */
+  }, [getInfo]);
+
+  if (!loaded) return <></>;
+
+  const { entrant, ladder } = state;
 
   return (
     <div className="wrapper">
       <div className="profile-picture">
         <img
-          src={CONFIG.avatar_source == "" ? "Avatar.png" : CONFIG.avatar_source}
-          style={{objectFit: 'contain', width: '100px', height: '100px'}} />
+          src={CONFIG.avatarSource == "" ? "Avatar.png" : CONFIG.avatarSource}
+          style={{ objectFit: "contain", width: "100px", height: "100px" }}
+        />
       </div>
 
       <div className="entrant-name">
-        {CONFIG.override_name == "" ? this.state.name : CONFIG.override_name}
+        {CONFIG.overrideName == "" ? entrant.name : CONFIG.overrideName}
       </div>
 
       <div className="entrant-info">
         <div className="entrant-id">
-          <div>ID: {state.id}</div>
+          <div>ID: {entrant.id}</div>
         </div>
         <div className="entrant-rank">
-          <div>Rank: {state.rank}</div>
+          <div>Rank: {entrant.rank}</div>
         </div>
         <div className="entrant-points">
           <div>RP:</div>
           <div />
-          <div>{state.rankingPoints}</div>
+          <div>{entrant.rankingPoints}</div>
         </div>
         <div className="entrant-points">
           <div>TP:</div>
           <div />
-          <div>{state.totalPoints}</div>
+          <div>{entrant.totalPoints}</div>
         </div>
       </div>
 
       <div className="clear-info">
         <div className="passes">
           <div>Passes:</div>
-          <div>{state.passes}</div>
+          <div>{entrant.totalPass}</div>
         </div>
         <div className="fcs">
           <div>FCs:</div>
-          <div>{state.fullCombos}</div>
+          <div>{entrant.totalFc}</div>
         </div>
         <div className="fecs">
           <div>FECs:</div>
-          <div>{state.fullExcellentCombos}</div>
+          <div>{entrant.totalFec}</div>
         </div>
         <div className="quads">
           <div>Quads:</div>
-          <div>{state.quadStars}</div>
+          <div>{entrant.totalQuad}</div>
         </div>
         <div className="quints">
           <div>Quints:</div>
-          <div>{state.quintStars}</div>
+          <div>{entrant.totalQuint}</div>
         </div>
       </div>
 
       <div className="tech-level-info">
         <div className="bracket">
           <div>BR:</div>
-          <div>{state.bracketLevel}</div>
+          <div>{entrant.bracketLevel}</div>
         </div>
         <div className="crossover">
           <div>XO:</div>
-          <div>{state.crossoverLevel}</div>
+          <div>{entrant.crossoverLevel}</div>
         </div>
         <div className="footswitch">
           <div>FS:</div>
-          <div>{state.footswitchLevel}</div>
+          <div>{entrant.footswitchLevel}</div>
         </div>
         <div className="jack">
           <div>JA:</div>
-          <div>{state.jackLevel}</div>
+          <div>{entrant.jackLevel}</div>
         </div>
         <div className="sideswitch">
           <div>SS:</div>
-          <div>{state.sideswitchLevel}</div>
+          <div>{entrant.sideswitchLevel}</div>
         </div>
         <div className="doublestep">
           <div>DS:</div>
-          <div>{state.doublestepLevel}</div>
+          <div>{entrant.doublestepLevel}</div>
         </div>
         <div className="stamina">
           <div>ST:</div>
-          <div>{state.staminaLevel}</div>
+          <div>{entrant.staminaLevel}</div>
         </div>
       </div>
-      
+
       <div className="ladder">
-        <div className="ladder-title">ITL Ladder</div>
-        {state.ladder.map((item, index) => {
+        <div className="ladder-title">ITL Online 2023 - Leaderboard</div>
+        {ladder.map((player, index) => {
           return (
-            <div key={index} className={item.type}>
-              <div className="ladder-rank">{`${item.rank} + '. ' + ${item.name}`}</div>
-              <div>{format_difference(item.difference)}</div>
+            <div key={index} className={player.type}>
+              <div className="ladder-rank">
+                {player.rank}. {player.name}
+              </div>
+              <div>{formatDifference(player.difference)}</div>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("entrant"));
-root.render(<ITLWidget />)
+root.render(<ITLWidget />);
